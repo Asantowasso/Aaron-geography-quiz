39f4d5a18f1cf1f97d035a1ba0781f69ebaf107f
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-09-03 14:14:16 -0400

    added a start button

---
+++ script.js
@@ -0,0 +1,7 @@
+//I need a start button which activates a timer
+// When the user answers a question it goes to the next question
+// A running timer that incurs a penalty when an incorrect answer is given
+// When the game is over I can enter my initals
+
+var answers = ["British Columbia", "Rome", "Cambodia", "The Amazon", "China" ]
+
