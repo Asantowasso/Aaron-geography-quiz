ea2af8549d2872ccf197d794377f3d88aa600fdb
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-09-04 13:12:42 -0400

    Added quiz questions as variables

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // When the user answers a question it goes to the next question
 // A running timer that incurs a penalty when an incorrect answer is given
 // When the game is over I can enter my initals
-var answers = ["British Columbia", "Rome", "Cambodia", "The Amazon", "China" ]
+var Correctanswer = ["British Columbia", "Rome", "Cambodia", "The Amazon", "China" ]
 var timerEl = document.getElementById("timer")
 function start (startTimer) {
     
@@ -10,6 +10,65 @@ function start (startTimer) {
 
 
 }
+//A list of my questions
+var quizQuestions = [
+{
+    question: "What is Canads's westernmost province",
+    answers: {
+    a: "Quebec",
+    b: "British Columbia",
+    c: "Ontario",
+    d: "Newfoundland and Labrador"
+    },
+    correctAnswer: "British Columbia"
+},
+
+
+
+{
+question: "What is the capitol of Italy",
+answers: {
+a: "Naples",
+b: "Turin",
+c: "Tyre",
+d: "Rome"
+},
+correctAnswer: "Rome"
+},
+
+{
+question: "What country is Angkor Wat located in",
+answers: {
+a: "Cambodia",
+b: "India",
+c: "Vietnam",
+d: "Laos"
+},
+correctAnswer: "Cambodia"
+},
+
+{
+question: "What is the longest river in the world?",
+answers: {
+a: "The Nile",
+b: "The Poe River",
+c: "The Amazon",
+d: "The Colorado"
+},
+correctAnswer: "The Amazon"
+},
+{
+question: "What is the most populated country in the world?",
+answers: {
+a: "China",
+b: "Indonesia",
+c: "Nigeria",
+d: "Pakistan"
+},
+correctAnswer: "China"
+}
+
+]
 //Timer lets user now how much timer they have to answer the question
 function startTimer(){
 var counter = 15;
