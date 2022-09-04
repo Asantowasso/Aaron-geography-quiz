69c35e8679e2f79e3ae117cc892b0064605ba94a
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-09-04 12:36:10 -0400

    functional timer

--- script.js
+++ script.js
@@ -2,28 +2,30 @@
 // When the user answers a question it goes to the next question
 // A running timer that incurs a penalty when an incorrect answer is given
 // When the game is over I can enter my initals
-var timerEl = document.getElementById("timer")
 var answers = ["British Columbia", "Rome", "Cambodia", "The Amazon", "China" ]
+var timerEl = document.getElementById("timer")
+function start (startTimer) {
+    
+    var counter = 15;
 
-function start () {
-    var randomIndex = Math.floor(Math.random() * answers.length);
 
-    var chosenAwnser = answers [randomIndex];
-    var givenAnswer = ""
-    var timeLeft= 10;
-
-startTimer();
 }
-
+//Timer lets user now how much timer they have to answer the question
 function startTimer(){
-var counter = 10;
-counter--;
- var timer = setInterval(function() {  
+var counter = 15;
 timerEl.textContent = counter;
 
+var timer = setInterval(function(){
+counter--;
+timerEl.textContent = counter;
+
+if (counter === 0) {
+    clearInterval(timer);
+}
 }, 1000);
 
-}
+
+
 
 document.getElementById("start").addEventListener
-("click", start)
+("click", start)}
