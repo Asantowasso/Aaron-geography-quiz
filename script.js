61083479b9d8faeb77aaa711fe16bf18e292f6bc
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-09-04 16:13:07 -0400

    Attempting to add functionality to quiz questions

--- script.js
+++ script.js
@@ -2,25 +2,27 @@
 // When the user answers a question it goes to the next question
 // A running timer that incurs a penalty when an incorrect answer is given
 // When the game is over I can enter my initals
-var Correctanswer = ["British Columbia", "Rome", "Cambodia", "The Amazon", "China" ]
+var correctAnswer = ["British Columbia", "Rome", "Cambodia", "The Amazon", "China" ]
+
+var incorrectAnswer = ["Quebec", "Ontario", "Newfoundland and Labrador", "Naples", "Turin", "Tyre", "India", "Vietname", "Laos","The Nile", "The Po River", "The Colorado", "Indonesia", "Nigeria", "Pakistan" ]
 var timerEl = document.getElementById("timer")
-function start (startTimer) {
+function startClock (startTimer) {
     
     var counter = 15;
 
 
 }
 //A list of my questions
-var quizQuestions = [
+const quizQuestions = [
 {
     question: "What is Canads's westernmost province",
     answers: {
-    a: "Quebec",
-    b: "British Columbia",
-    c: "Ontario",
-    d: "Newfoundland and Labrador"
+    a: 'Quebec',
+    b: 'British Columbia',
+    c: 'Ontario',
+    d: 'Newfoundland and Labrador'
     },
-    correctAnswer: "British Columbia"
+    correctAnswer: 'b'
 },
 
 
@@ -28,34 +30,34 @@ var quizQuestions = [
 {
 question: "What is the capitol of Italy",
 answers: {
-a: "Naples",
-b: "Turin",
-c: "Tyre",
-d: "Rome"
+a: 'Naples',
+b: 'Turin',
+c: 'Tyre',
+d: 'Rome'
 },
-correctAnswer: "Rome"
+correctAnswer: 'd'
 },
 
 {
 question: "What country is Angkor Wat located in",
 answers: {
-a: "Cambodia",
-b: "India",
-c: "Vietnam",
-d: "Laos"
+a: 'Cambodia',
+b: 'India',
+c: 'Vietnam',
+d: 'Laos'
 },
-correctAnswer: "Cambodia"
+correctAnswer: "a"
 },
 
 {
 question: "What is the longest river in the world?",
 answers: {
-a: "The Nile",
-b: "The Poe River",
-c: "The Amazon",
-d: "The Colorado"
+a: 'The Nile',
+b: 'The Po River',
+c: 'The Amazon',
+d: 'The Colorado'
 },
-correctAnswer: "The Amazon"
+correctAnswer: 'c'
 },
 {
 question: "What is the most populated country in the world?",
@@ -65,10 +67,14 @@ b: "Indonesia",
 c: "Nigeria",
 d: "Pakistan"
 },
-correctAnswer: "China"
+correctAnswer: 'a'
 }
 
 ]
+
+//Add fuctionallity to the questions
+
+
 //Timer lets user now how much timer they have to answer the question
 function startTimer(){
 var counter = 15;
@@ -82,6 +88,7 @@ if (counter === 0) {
     clearInterval(timer);
 }
 }, 1000);
+
 
 
 
