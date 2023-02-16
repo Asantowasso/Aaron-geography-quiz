9869690dd625891a165901563680982139db8a9b
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-02-15 23:30:17 -0500

    created outline

--- script.js
+++ script.js
@@ -1,111 +1,139 @@
-var counter = 90;
-var answerArr = [];
-var score = 0;
-var currentIndex = 0
+const Questions = [{
+  id: 0,
+  q: "What is capital of India?",
+  a: [{ text: "gandhinagar", isCorrect: false },
+      { text: "Surat", isCorrect: false },
+      { text: "Delhi", isCorrect: true },
+      { text: "mumbai", isCorrect: false }
+  ]
 
+},
+{
+  id: 1,
+  q: "What is the capital of Thailand?",
+  a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+      { text: "phuket", isCorrect: false },
+      { text: "Ayutthaya", isCorrect: false },
+      { text: "Bangkok", isCorrect: true }
+  ]
 
-//I need a start button which activates a timer
-// When the user answers a question it goes to the next question
-// A running timer that incurs a penalty when an incorrect answer is given
-// When the game is over I can enter my initals
-
-var timerEl = document.getElementById("timer");
-const quizQuestionsContainer = document.getElementById("question-container");
-var correctAnswercontainer = document.getElementById("correct-answers");
-var startcontainer = document.getElementById("start-container");
-//answers
-
-
-
-//A list of my questions
-var quizQuestionsarr = [
-  {
-    
-    question: "What is Canads's westernmost province",
-    answers: ["British Columbia", "Quebec", "Newfoundland and Labrador", "Ontartio"],
-    answer: "British Columbia"
-
-    
-  },
-
-  {
-    question: "What is the capital of Italy",
-    answers: ["Naples", "Turin", "Tyre", "Rome"],
-    answer: "Rome"
-  },
-   
-
-  {
-    question: "What country is Angkor Wat located in",
-    answers: ["Cambodia", "India", "Vietnam", "Laos"],
-    answer: "Cambodia"
-  },
-
-  {
-    question: "What is the longest river in the world?",
-    answers: ["The Nile", "The Amazon", "The Colorado", "The Po River"],
-    answer: "The Amazon"
-  },
-  {
-    question: "What is the most populated country in the world?",
-    answers: ["Indonesia","Pakistan", "China","Nigeria"],
-    answer: "China"
-  },
-];
-
-//Timer lets user now how much timer they have to answer the question
-function start() {
-  timerEl.textContent = counter;
-
-  var timer = setInterval(function () {
-    counter--;
-    timerEl.textContent = counter;
-
-    if (counter === 0) {
-      clearInterval(timer);
-    }
-  }, 90,000);
-
-
-
-
-  startcontainer.classList.add("hidden");
-  displayQuestion() 
-}
-//var currentIndex = 0
-//Add fuctionallity to the questions
-function displayQuestion() {
-  var currentQObj = quizQuestionsarr[currentIndex]
-    quizQuestionsContainer.innerHTML = "";    
-    var h1El = document.createElement("p");
-    h1El.textContent = currentQObj.question;    
-    quizQuestionsContainer.append(h1El);
-
-  for (var i = 0; i < currentQObj.answers.length; i++) {
-    var h2El = document.createElement("button");
-    h2El.textContent = currentQObj.answers[i];
-    quizQuestionsContainer.append(h2El);
-    h2El.addEventListener('click', checkAnswer)
+},
+{
+  id: 2,
+  q: "What is the capital of Gujarat",
+  a: [{ text: "surat", isCorrect: false },
+      { text: "vadodara", isCorrect: false },
+      { text: "gandhinagar", isCorrect: true },
+      { text: "rajkot", isCorrect: false }
+  ]
 
 }
 
+]
+
+// Set start
+var start = true;
+
+// Iterate
+function iterate(id) {
+
+// Getting the result display section
+var result = document.getElementsByClassName("result");
+result[0].innerText = "";
+
+// Getting the question
+const question = document.getElementById("question");
+
+
+// Setting the question text
+question.innerText = Questions[id].q;
+
+// Getting the options
+const op1 = document.getElementById('op1');
+const op2 = document.getElementById('op2');
+const op3 = document.getElementById('op3');
+const op4 = document.getElementById('op4');
+
+
+// Providing option text 
+op1.innerText = Questions[id].a[0].text;
+op2.innerText = Questions[id].a[1].text;
+op3.innerText = Questions[id].a[2].text;
+op4.innerText = Questions[id].a[3].text;
+
+// Providing the true or false value to the options
+op1.value = Questions[id].a[0].isCorrect;
+op2.value = Questions[id].a[1].isCorrect;
+op3.value = Questions[id].a[2].isCorrect;
+op4.value = Questions[id].a[3].isCorrect;
+
+var selected = "";
+
+// Show selection for op1
+op1.addEventListener("click", () => {
+  op1.style.backgroundColor = "lightgoldenrodyellow";
+  op2.style.backgroundColor = "lightskyblue";
+  op3.style.backgroundColor = "lightskyblue";
+  op4.style.backgroundColor = "lightskyblue";
+  selected = op1.value;
+})
+
+// Show selection for op2
+op2.addEventListener("click", () => {
+  op1.style.backgroundColor = "lightskyblue";
+  op2.style.backgroundColor = "lightgoldenrodyellow";
+  op3.style.backgroundColor = "lightskyblue";
+  op4.style.backgroundColor = "lightskyblue";
+  selected = op2.value;
+})
+
+// Show selection for op3
+op3.addEventListener("click", () => {
+  op1.style.backgroundColor = "lightskyblue";
+  op2.style.backgroundColor = "lightskyblue";
+  op3.style.backgroundColor = "lightgoldenrodyellow";
+  op4.style.backgroundColor = "lightskyblue";
+  selected = op3.value;
+})
+
+// Show selection for op4
+op4.addEventListener("click", () => {
+  op1.style.backgroundColor = "lightskyblue";
+  op2.style.backgroundColor = "lightskyblue";
+  op3.style.backgroundColor = "lightskyblue";
+  op4.style.backgroundColor = "lightgoldenrodyellow";
+  selected = op4.value;
+})
+
+// Grabbing the evaluate button
+const evaluate = document.getElementsByClassName("evaluate");
+
+// Evaluate method
+evaluate[0].addEventListener("click", () => {
+  if (selected == "true") {
+      result[0].innerHTML = "True";
+      result[0].style.color = "green";
+  } else {
+      result[0].innerHTML = "False";
+      result[0].style.color = "red";
+  }
+})
 }
 
-function checkAnswer(e){
-console.log(e.target.textContent, "textContent")
-var contents = e.target.textContent
-const correctAnswerIndex = quizQuestionsarr[currentIndex].answer
-
-
-    if(contents === correctAnswerIndex) {
-        score++;
-        alert ("correct")
-}
-else {
-    alert ("incorrect")
+if (start) {
+iterate("0");
 }
 
+// Next button and method
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
+
+next.addEventListener("click", () => {
+start = false;
+if (id < 2) {
+  id++;
+  iterate(id);
+  console.log(id);
 }
 
-
-document.getElementById("start").addEventListener("click", start);
+})
