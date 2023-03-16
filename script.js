4538d5afec276d75a863ce336c9835103623aef3
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-03-15 22:35:10 -0400

    added styling

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const resultsContainer = document.getElementById("results");
 
 const submitButton = document.getElementById("submit");
 
+
+
+
 // An array for the questions
 const myQuestions = [
   {
@@ -61,8 +64,10 @@ function buildQuiz() {
     }
 
     output.push(
-      `<div class"question"> ${currentQuestion.question} </div>
-      <div class="answers"> ${answers.join('')} </div>`
+      `<div class="slide">
+      <div class"question"> ${currentQuestion.question} </div>
+      <div class="answers"> ${answers.join("")} </div>
+        </div>`
     );
   }
 );
@@ -93,13 +98,55 @@ function showResults() {
       answerContainers[questionNumber].style.color = "red";
     }
     });
-      resultsContainer.innerHTML = `${numCorrect} our of ${myQuestions.length}`;
+      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
     }
   
+
+    function showSlide(n){
+      slides[currentSlide].classList.remove('active-slide');
+      slides[n].classList.add('active-slide');
+      currentSlide = n;
+      if(currentSlide === 0){
+        previousButton.style.display = 'none';
+      }
+      else{
+        previousButton.style.display = 'inline-block'
+      }
+      if(currentSlide === slides.length-1){
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block'
+      }
+      else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+      }
+
+    }
+
+    function showNextSlide() {
+      showSlide(currentSlide + 1);
+    }
+
+    function showPreviousSlide() {
+      showSlide(currentSlide - 1);
+    }
 
 
 //A function to create the quiz
 buildQuiz();
 
+const previousButton = document.getElementById("previous");
+
+const nextButton = document.getElementById("next")
+
+const slides = document.querySelectorAll(".slide")
+
+let currentSlide = 0;
+
+showSlide(currentSlide);
+
 submitButton.addEventListener("click", showResults);
+previousButton.addEventListener("click", showPreviousSlide)
+nextButton.addEventListener("click", showNextSlide)
+
 
